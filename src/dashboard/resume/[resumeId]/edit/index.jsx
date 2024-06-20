0567bc304d0949394c9de3b2981500f3391bdaab
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-06-20 22:24:22 +0530

    Resume Preview Section Updated

--- src/dashboard/resume/[resumeId]/edit/index.jsx
+++ src/dashboard/resume/[resumeId]/edit/index.jsx
@@ -1,16 +1,26 @@
-import React, { useEffect } from 'react'
+import FormSection from '@/components/FormSection';
+import ResumePreview from '@/components/ResumePreview';
+import { ResumeInfoContext } from '@/context/ResumeInfoContext';
+import dummy from '@/data/dummy';
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 
 const EditResume = () => {
 
   const params = useParams();
+  const [resumeInfo, setResumeInfo] = useState();
 
   useEffect(() => {
-    console.log(params.resumeId)
+    setResumeInfo(dummy)
   }, [])
 
   return (
-    <div>EditResume</div>
+    <ResumeInfoContext.Provider value={{resumeInfo, setResumeInfo}}>
+      <div className='grid grid-cols-1 md:grid-cols-2'>
+        <FormSection />
+        <ResumePreview />
+      </div>
+    </ResumeInfoContext.Provider>
   )
 }
 
